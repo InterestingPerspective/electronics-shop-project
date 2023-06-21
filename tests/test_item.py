aeeea7f26d8b7162c869989b930fc00eb8e59aeb
Lev Polevshchikov <upmindset.28@gmail.com>
2023-06-21 23:01:55 +0300

    homework_1

--- tests/test_item.py
+++ tests/test_item.py
@@ -1,1 +1,17 @@
-"""Здесь надо написать тесты с использованием pytest для модуля item."""
+import pytest
+from src.item import Item
+
+
+@pytest.fixture
+def coll():
+    return Item("Смартфон", 10000, 20)
+
+
+def test_calculate_total_price(coll):
+    assert coll.calculate_total_price() == 200000
+
+
+def test_apply_discount(coll):
+    Item.pay_rate = 0.8
+    coll.apply_discount()
+    assert coll.price == 8000.0
